b05a1dad533698680d1cfc51ac3d622c4e3b8c01
Pedro de Camargo Zago <pedro.camargozago@hotmail.com.br>
2023-02-17 15:02:35 -0300

    Streams no Node.js - Criando stream de leitura

---
+++ streams/fundamentals.js
@@ -0,0 +1,25 @@
+import process from 'node:process';
+import { Readable } from 'node:stream';
+
+// process.stdin.pipe(process.stdout);
+
+class OneToHundredStream extends Readable {
+  index = 1
+
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null)
+      } else {
+        const buffer = Buffer.from(String(i))
+
+        this.push(buffer)
+      }
+    }, 100)
+  }
+}
+
+new OneToHundredStream()
+  .pipe(process.stdout)
